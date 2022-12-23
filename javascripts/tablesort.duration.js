debf1eaa8207cb1438afc9ce1654eafeb9db8383
notegone <dmwchiu@gmail.com>
2022-12-22 23:53:08 -0500

    Deployed dcddab3 with MkDocs version: 1.3.0

---
+++ javascripts/tablesort.duration.js
@@ -0,0 +1,31 @@
+(function(){
+  var cleanNumber = function(i) {
+    return i.replace(/[^\-?0-9.]/g, '');
+  },
+
+  compareNumber = function(a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+
+    a = isNaN(a) ? 0 : a;
+    b = isNaN(b) ? 0 : b;
+
+    return a - b;
+  };
+
+  Tablesort.extend('number', function(item) {
+    return item.match(/^[-+]?[£\x24Û¢´€]?\d+\s*([,\.]\d{0,2})/) || // Prefixed currency
+      item.match(/^[-+]?\d+\s*([,\.]\d{0,2})?[£\x24Û¢´€]/) || // Suffixed currency
+      item.match(/^[-+]?(\d)*-?([,\.]){0,1}-?(\d)+([E,e][\-+][\d]+)?%?$/); // Number
+  }, function(a, b, col) {
+	  
+	// Check if the column name is "Timestamp" or "Text Segment"
+	if (col === 'Timestamp' || col === 'Text Segment') {
+		return 0; // Don't sort if the column name matches these values
+	}
+    a = cleanNumber(a);
+    b = cleanNumber(b);
+
+    return compareNumber(b, a);
+  });
+}());
--- javascripts/tablesort.js
+++ javascripts/tablesort.js
@@ -1,6 +1,12 @@
 document$.subscribe(function() {
   var tables = document.querySelectorAll("article table:not([class])")
   tables.forEach(function(table) {
-    new Tablesort(table)
+    var headers = table.querySelectorAll("th")
+    var headerText = Array.prototype.map.call(headers, function(th) {
+      return th.textContent
+    })
+    if (headerText.indexOf("Timestamp") === -1 && headerText.indexOf("Text Segment") === -1) {
+      new Tablesort(table)
+    }
   })
 })
